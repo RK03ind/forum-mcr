6a3c5f47ab926537e79c297a7abeb2f270ede489
RK03ind <gamitech95@gmail.com>
2023-06-24 19:35:09 +0530

    ADD: Some npm packages

---
+++ src/shared/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import "./Sidebar.scss";
+const Sidebar = () => {
+  return (
+    <aside>
+      <div className="active">Home </div>
+      <div>Explore</div>
+      <div>Bookmarks</div>
+      <div>Profile</div>
+    </aside>
+  );
+};
+export default Sidebar;
